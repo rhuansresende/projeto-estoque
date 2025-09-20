3de1357bf079bb85d36fa8650630ec66086a77f4
Rhuan Resende <rhuan.resende@detran.go.gov.br>
2025-09-20 19:06:18 -0300

    fix(): Ajustes arquivos

--- start_estoque_prod.bat
+++ start_estoque_prod.bat
@@ -4,10 +4,11 @@ echo Iniciando sistema de estoque...
 echo ================================
 
 set ENV=prod
+cd /d "D:\Documentos\Rhuan\AmbienteEstoque\Projetos\projeto-estoque"
 
 echo.
 echo Subindo containers para o ambiente: %ENV%
-"C:\Program Files\Git\bin\bash.exe" --login -i "D:\Documentos\Rhuan\AmbienteEstoque\Projetos\projeto-estoque\start.sh" %ENV%
+"C:\Program Files\Git\bin\bash.exe" --login -i "./start.sh" %ENV%
 
 echo.
 echo ================================
