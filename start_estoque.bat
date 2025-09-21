ab93cf78478a84513d4afb191e768f203dc10c03
Rhuan Resende <rhuan.resende@detran.go.gov.br>
2025-09-20 21:29:58 -0300

    fix(): Ajustes arquivos

--- start_estoque.bat
+++ start_estoque.bat
@@ -23,10 +23,52 @@ if "%ENV%"=="" (
     goto MENU
 )
 
+set BASE_PATH=D:\Documentos\Rhuan\AmbienteEstoque\Projetos
+
+echo.
+echo Atualizando repositórios Git...
+
+:: Estoque Backup
+cd /d %BASE_PATH%\estoque-backup
+if exist .git (
+  echo Atualizando Estoque Backup...
+  git pull
+) else (
+  echo [AVISO] estoque-backup nao é um repositório Git
+)
+
+:: Estoque Service
+cd /d %BASE_PATH%\estoque-service
+if exist .git (
+  echo Atualizando Estoque Service...
+  git pull
+) else (
+  echo [AVISO] estoque-service nao é um repositório Git
+)
+
+:: Estoque WEB
+cd /d %BASE_PATH%\estoque-web
+if exist .git (
+  echo Atualizando Estoque WEB...
+  git pull
+) else (
+  echo [AVISO] estoque-web nao é um repositório Git
+)
+
+:: Projeto Estoque
+cd /d %BASE_PATH%\projeto-estoque
+if exist .git (
+  echo Atualizando Projeto Estoque...
+  git pull
+) else (
+  echo [AVISO] projeto-estoque nao é um repositório Git
+)
+
 echo.
 echo Subindo containers para o ambiente: %ENV%
-"C:\Program Files\Git\bin\bash.exe" --login -i "D:\Documentos\Rhuan\AmbienteEstoque\Projetos\projeto-estoque\start.sh" %ENV%
+"C:\Program Files\Git\bin\bash.exe" --login -i "%BASE_PATH%\projeto-estoque\start.sh" %ENV%
 
 echo.
 echo ================================
 echo Fim do processo.
+pause
--- start_estoque_prod.bat
+++ start_estoque_prod.bat
@@ -4,12 +4,31 @@ echo Iniciando sistema de estoque...
 echo ================================
 
 set ENV=prod
-cd /d "D:\Documentos\Rhuan\AmbienteEstoque\Projetos\projeto-estoque"
+set BASE_PATH=D:\Documentos\Rhuan\AmbienteEstoque\Projetos
+
+:: Lista de projetos para atualizar
+set PROJECTS=estoque-backup estoque-service estoque-web projeto-estoque
+
+echo.
+echo Atualizando repositórios Git...
+
+for %%P in (%PROJECTS%) do (
+    echo ------------------------
+    echo Atualizando projeto %%P
+    cd /d %BASE_PATH%\%%P
+    if exist .git (
+        git pull
+    ) else (
+        echo [AVISO] %%P nao é um repositório Git
+    )
+)
 
 echo.
 echo Subindo containers para o ambiente: %ENV%
+cd /d %BASE_PATH%\projeto-estoque
 "C:\Program Files\Git\bin\bash.exe" --login -i "./start.sh" %ENV%
 
 echo.
 echo ================================
 echo Fim do processo.
+pause
